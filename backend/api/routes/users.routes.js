943d8c9291161eed1ff9e5a19d0f9bf63cc560ed
aguusoleti <agustin_soleti@hotmail.com>
2023-03-24 15:46:15 -0300

    user utils

--- backend/api/routes/users.routes.js
+++ backend/api/routes/users.routes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { userProfile} from "../controllers/user.controller.js";
-
+import { userProfile, newUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+router.post("/new", newUser);
 
-router.get("/profile",userProfile)
-
+router.get("/profile", userProfile);
 
 export default router;
