3e4b797b975dc9a966def1b2effdf93b70d80857
pccomponentshenry <rafael84barreiro@gmail.com>
2023-03-30 16:30:13 -0300

    star json projects

--- backend/api/models/projects.js
+++ backend/api/models/projects.js
@@ -41,6 +41,9 @@ const userSchema = mongoose.mongoose.Schema({
     devRequired: {
         type:[String],
     },
+    techRequired:{
+        type: [String]
+    },
     colaborators:{
         type:[String],
     },
