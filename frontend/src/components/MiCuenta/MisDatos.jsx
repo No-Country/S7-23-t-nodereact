1d4d3790828b6424f79094e5034c72cea3090c5d
Juanse Giannattasio <juanse.giannattasio@gmail.com>
2023-04-03 23:51:17 -0300

    Create component "MiCuenta"

---
+++ frontend/src/components/MiCuenta/MisDatos.jsx
@@ -0,0 +1,10 @@
+const MisDatos = () => {
+    return (
+        <div>
+            Aca imagen y navegacion // links a perfil - campaña- financiamiento
+        </div>
+      
+    );
+  };
+  
+  export default MisDatos;
