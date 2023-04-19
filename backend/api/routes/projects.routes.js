4f25be5161c50f442e43a75dff37dbda3c807f15
Tomas <tomasgarciagonzalo@gmail.com>
2023-04-18 20:23:10 -0400

    deploy back, successes comentados

--- backend/api/routes/projects.routes.js
+++ backend/api/routes/projects.routes.js
@@ -5,7 +5,7 @@ import {
   getProjectById,
   updateProject,
   PayCard,
-  Success,
+  // Success,
 } from "../controllers/project.controller.js";
 
 const router = express.Router();
@@ -15,7 +15,7 @@ router.post("/", postProject);
 router.get("/", getProjects);
 router.get("/:id", getProjectById);
 router.post("/pay/:id", PayCard);
-router.get("/success/:id/:amount", Success);
+// router.get("/success/:id/:amount", Success);
 
 router.put("/", updateProject);
 
